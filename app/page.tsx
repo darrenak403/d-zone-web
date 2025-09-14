3afbd11ef42b93d2c46351ed23d1b472e21d9e2e
darrenak403 <ngothanhdat4002@gmail.com>
2025-09-14 16:04:44 +0700

    feat: update Home component to include UserButton and apply auth layout styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,10 @@
-import Image from "next/image";
+import {UserButton} from "@clerk/nextjs";
+import styles from "../styles/authLayout.module.css";
 
 export default function Home() {
-  return <div>Subcrrai</div>;
+  return (
+    <main className={styles.main}>
+      <UserButton />
+    </main>
+  );
 }
